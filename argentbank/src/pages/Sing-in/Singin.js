d17ef3c45ecb46fef24fa28cfda2aa8eaed90868
geronimo3tr <jocelyn.lepagepro@gmail.com>
2024-02-01 16:11:38 +0100

    sing up page

--- argentbank/src/pages/Sing-in/Singin.js
+++ argentbank/src/pages/Sing-in/Singin.js
@@ -1,8 +1,27 @@
 function Singin() {
   return (
     <>
-      <main className="test">
-        <h1>vsdop</h1>
+      <main className="dark-background">
+        <section className="sing-in-card">
+          <h1>Sing In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label for="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label for="username">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            <a href="./user.html" class="sign-in-button">
+              Sign In
+            </a>
+          </form>
+        </section>
       </main>
     </>
   );
