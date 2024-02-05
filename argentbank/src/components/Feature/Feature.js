cf4d8e0678fa1a343cc84c3325a7ea6c10239e49
geronimo3tr <jocelyn.lepagepro@gmail.com>
2024-02-05 15:11:33 +0100

    featyre fix

--- argentbank/src/components/Feature/Feature.js
+++ argentbank/src/components/Feature/Feature.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Icon(props) {
+function Feature(props) {
   return (
     <>
       <div className="icon-container">
-        <img className="icon" src={props.icon} alt="test" />
+        <img className="icon" src={props.icon} alt={props.alt} />
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </div>
     </>
   );
 }
-export default Icon;
+export default Feature;
--- argentbank/src/pages/Home/Home.js
+++ argentbank/src/pages/Home/Home.js
@@ -1,26 +1,44 @@
 import React from "react";
 import background from "../../images/bank-tree.jpeg";
-import Icon from "../../components/Icon/Icon";
-import Card from "../../components/Card/Card";
-import data from "../../data.json";
+import Feature from "../../components/Feature/Feature";
+import chat from "../../images/icon-chat.png";
+import money from "../../images/icon-money.png";
+import security from "../../images/icon-security.png";
 
 function Home() {
-  console.log(data.cards);
   return (
     <>
       <main>
         <section>
           <img className="background" src={background} alt="plant" />
-          <article className="card">
-            {data.cards.map((card, index) => (
-              <Card key={index} title={card.title} description={card.description} />
-            ))}
+          <article className="bank-promotion">
+            <div className="highlight">
+              <span>No fees.</span>
+              <span>No minimum deposit.</span>
+              <span>High interest rates.</span>
+            </div>
+            <p className="highlight-description">Open a savings account with Argent Bank today!</p>
           </article>
         </section>
         <section className="icons-container">
-          {data.icons.map((icon, index) => (
-            <Icon key={index} icon={icon.icon} title={icon.title} description={icon.description} />
-          ))}
+          <Feature
+            icon={chat}
+            title="You are our #1 priority"
+            description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+            alt="icon-chat"
+          />
+          <Feature
+            icon={money}
+            title="More savings means higher rates"
+            description="The more you save with us, the higher your interest rate will be!"
+            alt="icon-money"
+          />
+          <Feature
+            icon={security}
+            title="Security you can trust"
+            description="We use top of the line encryption to make sure your data and money is always safe."
+            alt="icon-security"
+          />
         </section>
       </main>
     </>
